ccb4cdab0a13ca9544cdfd2741f07e8a40a7abba
ShurutiThinkQ <tqshuruti@gmail.com>
2026-06-27 18:40:15 +0530

    New repository by Shuruti

---
+++ home.js
@@ -0,0 +1,4 @@
+console.log("Home page: Line1");
+console.log("Home page: Line2");
+console.log("Home page: Line3");
+
---
+++ login.js
@@ -0,0 +1,3 @@
+console.log("Login page: Line1");
+console.log("Login page: Line2");
+console.log("Login page: Line3");
